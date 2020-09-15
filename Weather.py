69f2928ad39bfadbb83c1b36578b05dd95832c8b
Tahjei <60302507+Tahjei@users.noreply.github.com>
2020-09-15 00:35:38 -0400

    Update Weather.py
    added 'Prediction' class
    added 'optimization' function that writes to a new file
    added 'predict' function that recreates decision tree for new recommendations
    added comments
    improved readability

--- Weather.py
+++ Weather.py
@@ -12,7 +12,7 @@ final_url = base_url + "appid=" + api_key + "&zip=" + zip_code + "&units=imperia
 # This variable contains the JSON data which the API returns
 weather_data = requests.get(final_url).json()
 
-# Weather variables for test
+# Weather variables for testing, wind_speed and temp are the only active ones currently
 wind_speed = weather_data['wind']['speed']
 temp = weather_data['main']['temp']
 pressure = weather_data['main']['pressure']
@@ -36,32 +36,40 @@ class Prediction:
         self.temp_clf.predict_proba([[temp, wind_speed]])
         print(self.temp_clf.predict([[temp, wind_speed]]))
 
-    # def predict(self, temp, wind_speed):
-    #     # This is the data that will supervise our decision tree
-    #     weather = pd.read_csv("WeatherData.csv", sep=",")
-    #     x = weather.values[:, 0:2]
-    #     y = weather.values[:, 2:].reshape(-1, 1)
-    #
-    #     # decision tree classifier
-    #     self.temp_clf = DecisionTreeClassifier(max_depth=3)
-    #     self.temp_clf.fit(x, y)
-    #     self.temp_clf.predict_proba([[temp, wind_speed]])
-    #     print(self.temp_clf.predict([[temp, wind_speed]]))
-    #     # predict = self.temp.predict([[temp, wind_speed]])
-    #     return predict
+# This function is meant to update the decision tree based on what the user prefers.
+# It needs to be redone. Right now it throws an error
+    def predict(self, temp, wind_speed):
+        # This is the data that will supervise our decision tree
+        weather = pd.read_csv("WeatherData.csv", sep=",")
+        x = weather.values[:, 0:2]
+        y = weather.values[:, 2:].reshape(-1, 1)
 
+        # decision tree classifier
+        self.temp_clf = DecisionTreeClassifier(max_depth=3)
+        self.temp_clf.fit(x, y)
+        self.temp_clf.predict_proba([[temp, wind_speed]])
+        print(self.temp_clf.predict([[temp, wind_speed]]))
+        predict = self.temp.predict([[temp, wind_speed]])
+        return predict
+
+# The optimization function allows users to comment on how the program did when guessing
+# It then saves a new updated guess that is added to a new file to update the decision tree
     def optimization(self):
         rec = input("Was the recommendation good? Y/N ")
 
+# Creates a new file called 'newData' to hold the updated recommendations
         with open('newData.csv', 'w') as csvfile:
             filewriter = csv.writer(csvfile, delimiter='|', quoting=csv.QUOTE_MINIMAL)
-            # if rec == "Y" or "y" or "yes" or "Yes":
             if (rec.lower() == 'yes') or (rec.lower() == 'y'):
+                # This line writes the 'temperature', 'wind speed', and 'the clothing recommendation' to the new file
                 filewriter.writerow([self.temp, wind_speed, self.temp_clf.predict([[self.temp, wind_speed]])])
                 filewriter.writerow("\n")
             elif (rec.lower() == 'no') or (rec.lower() == 'n'):
                 change = input("Was the weather too hot or too cold? H/C ")
                 if (change.lower() == 'hot') or (change.lower() == 'h'):
+                    # The temp += 10 is meant to increase the bracket for the recommendation
+                    # however in the current form it only changes what temp is written to the file
+                    # the actual recommendation stays the same
                     self.temp += 10
                     filewriter.writerow([self.temp, wind_speed, self.temp_clf.predict([[self.temp, wind_speed]])])
                     filewriter.writerow("\n")
