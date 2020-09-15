860123d0827406368049512a1290ebd182938335
Tahjei <60302507+Tahjei@users.noreply.github.com>
2020-09-15 11:02:46 -0400

    Update Weather.py
    Predict class made functional
    The program now adds to the newData.csv file instead of replacing the contents
    Comments removed
    Comments added
    lines refactored

--- Weather.py
+++ Weather.py
@@ -1,14 +1,3 @@
-from sklearn.tree import DecisionTreeClassifier
-import requests
-import pandas as pd
-import csv
-
-# This stores the url this will ask the user to enter city ID and adds to the URL
-api_key = "35221e928de70d1425a6dae04b36e346"
-base_url = "http://api.openweathermap.org/data/2.5/weather?"
-zip_code = input("Enter a zip code: ")
-final_url = base_url + "appid=" + api_key + "&zip=" + zip_code + "&units=imperial"
-
 # This variable contains the JSON data which the API returns
 weather_data = requests.get(final_url).json()
 
@@ -34,23 +23,21 @@ class Prediction:
         self.temp_clf = DecisionTreeClassifier(max_depth=3)
         self.temp_clf.fit(x, y)
         self.temp_clf.predict_proba([[temp, wind_speed]])
-        print(self.temp_clf.predict([[temp, wind_speed]]))
+        print(temp, self.temp_clf.predict([[temp, wind_speed]]))
 
-# This function is meant to update the decision tree based on what the user prefers.
-# It needs to be redone. Right now it throws an error
-    def predict(self, temp, wind_speed):
+    def predict(self, temp):
         # This is the data that will supervise our decision tree
         weather = pd.read_csv("WeatherData.csv", sep=",")
         x = weather.values[:, 0:2]
         y = weather.values[:, 2:].reshape(-1, 1)
 
         # decision tree classifier
-        self.temp_clf = DecisionTreeClassifier(max_depth=3)
-        self.temp_clf.fit(x, y)
-        self.temp_clf.predict_proba([[temp, wind_speed]])
-        print(self.temp_clf.predict([[temp, wind_speed]]))
-        predict = self.temp.predict([[temp, wind_speed]])
-        return predict
+        self.predict_clf = DecisionTreeClassifier(max_depth=3)
+        self.predict_clf.fit(x, y)
+        self.predict_clf.predict_proba([[temp, wind_speed]])
+        self.predict_clf.predict_proba([[temp, wind_speed]])
+        print(temp, self.predict_clf.predict([[temp, wind_speed]]))
+        return self.predict_clf.predict([[temp, wind_speed]])
 
 # The optimization function allows users to comment on how the program did when guessing
 # It then saves a new updated guess that is added to a new file to update the decision tree
@@ -58,24 +45,20 @@ class Prediction:
         rec = input("Was the recommendation good? Y/N ")
 
 # Creates a new file called 'newData' to hold the updated recommendations
-        with open('newData.csv', 'w') as csvfile:
+        with open('newData.csv', 'a', newline='') as csvfile:
             filewriter = csv.writer(csvfile, delimiter='|', quoting=csv.QUOTE_MINIMAL)
             if (rec.lower() == 'yes') or (rec.lower() == 'y'):
                 # This line writes the 'temperature', 'wind speed', and 'the clothing recommendation' to the new file
-                filewriter.writerow([self.temp, wind_speed, self.temp_clf.predict([[self.temp, wind_speed]])])
-                filewriter.writerow("\n")
+                filewriter.writerow([self.temp, wind_speed, Prediction.predict(self, self.temp)])
             elif (rec.lower() == 'no') or (rec.lower() == 'n'):
                 change = input("Was the weather too hot or too cold? H/C ")
                 if (change.lower() == 'hot') or (change.lower() == 'h'):
                     # The temp += 10 is meant to increase the bracket for the recommendation
-                    # however in the current form it only changes what temp is written to the file
-                    # the actual recommendation stays the same
                     self.temp += 10
-                    filewriter.writerow([self.temp, wind_speed, self.temp_clf.predict([[self.temp, wind_speed]])])
-                    filewriter.writerow("\n")
+                    filewriter.writerow([self.temp, wind_speed, Prediction.predict(self, self.temp)])
                 elif (change.lower() == 'cold') or (change.lower() == 'c'):
-                    filewriter.writerow([self.temp, wind_speed, self.temp_clf.predict([[self.temp, wind_speed]])])
-                    filewriter.writerow("\n")
+                    self.temp -= 10
+                    filewriter.writerow([self.temp, wind_speed, Prediction.predict(self, self.temp)])
             else:
                 print("Have A Nice Day!")
 
